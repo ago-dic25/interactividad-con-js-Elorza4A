c3e2456a92d1b2a1114a03d49cc8a6b44801e44f
ALEXIS FELIPE ELORZA OBREGON <alexis.elorzaobr@uanl.edu.mx>
2025-09-26 22:38:20 -0600

    Update: "Dark mode", "Galeria"

--- index.js
+++ index.js
@@ -28,30 +28,78 @@ suma(10, 20);
 suma(100, 200);
 
 
-//===Elementos de la pagina como variables===///
-var botonBuscar = document.getElementById("buscar");
-
-console.log(botonBuscar);
-
-//==== Eventos===== //
-botonBuscar.addEventListener("click", function(){
-    //alert("Hiciste clic al boton");
-    botonBuscar.classList.add("rojo");
-});
-
-
 // === Variables Ejercicio de Clases === ///
 var mas = document.getElementById("mas");
 var verTodos = document.getElementById("verTodos");
 var verFotos = document.getElementById("verFotos");
-var posts = document.getElementById("posts")
+var posts = document.getElementById("posts");
+var post = document.querySelectorAll("div.post")
+var botonBuscar = document.getElementById("buscar");
+var busqueda = document.getElementById("txtBusqueda");
+var botonModo = document.getElementById("btnModo");
+var modo = document.querySelectorAll(".modo");
+console.log(modo)
+// === EVENTOS ==== //
+
+
+// modo oscuro
+botonModo.addEventListener("click", function(){
+    if(botonModo.innerText === "🌙"){
+        botonModo.innerText = "☀️";
+    }
+    else{
+        botonModo.innerText = "🌙";
+    }
+
+    modo.forEach(m => {
+        if (m.classList.contains("light")){
+            m.classList.add("dark");
+            m.classList.remove("light");
+        }
+        else{
+            m.classList.remove("dark");
+            m.classList.add("light");
+        }
+    });
+});
+
+
+// muestra el textbox de busqueda
+botonBuscar.addEventListener("click", function(){
+    if(busqueda.classList.contains("ocultar")){
+        busqueda.classList.remove("ocultar");
+        busqueda.classList.add("mostrar");
+    }
+    else{
+        busqueda.classList.remove("mostrar");
+        busqueda.classList.add("ocultar");
+    }
+    
+});
+
+// realiza la busqueda
+busqueda.addEventListener("keydown", function(tecla){
+    if(tecla.key === "Enter"){
+        post.forEach(div => {
+            if (div.textContent.includes(busqueda.value)){
+                div.classList.add("mostrar");
+                div.classList.remove("ocultar");
+            }
+            else{
+                div.classList.remove("mostrar");
+                div.classList.add("ocultar");
+            }
+        });
+    }
+})
+
 
 
 var x = 1;
 // Eventos de cada elemento
 mas.addEventListener("click", function(){
     // mostrar alerta en boton de +
-    alert("Sección en proceso... " + x);
+    alert("Conteo: " + x);
     x = x + 1;
 })
 
@@ -65,3 +113,36 @@ verTodos.addEventListener("click", function(){
         posts.classList.add("ocultar");
     }
 })
+
+var seePicks = document.getElementById("seeAll");
+var imagen = document.getElementById("imagen");
+var imagenes = ["imagenes/rachel-coyne-mTsotT-gMrY-unsplash.jpg",
+    "imagenes/soundtrap-n30_i7mx62o-unsplash.jpg",
+    "imagenes/jukka-aalho-OaPksPcVp50-unsplash.jpg"
+];
+
+// galería
+var i = 0;
+
+function cambiarImagen() {
+    var url = "url(" + imagenes[i] + ")";
+    imagen.style.backgroundImage = url;
+    i = i + 1;
+    if (i >= imagenes.length) {
+        i = 0;
+    }
+}
+
+var intervalo = null;
+
+seePicks.addEventListener("mouseover", function () {
+
+    if (intervalo === null) {
+        intervalo = setInterval(cambiarImagen, 3000);
+    }
+});
+
+seePicks.addEventListener("mouseout", function () {
+    clearInterval(intervalo);
+    intervalo = null; // importante reiniciarlo
+});
